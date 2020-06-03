7e087b72e06b26ce652201f655fd6ad0fb508c35
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2020-06-04 00:09:24 +0300

    fixed

--- js/task-10.js
+++ js/task-10.js
@@ -1,17 +1,17 @@
 import users from '../users.js';
 
 const getSortedUniqueSkills = users =>
-  users.reduce((calc, user) => {
-    user.skills.forEach(skill => {
-      if (!calc.includes(skill)) {
-        calc.push(skill);
-      }
-    });
+  users
+    .reduce((calc, user) => {
+      user.skills.forEach(skill => {
+        if (!calc.includes(skill)) {
+          calc.push(skill);
+        }
+      });
 
-    calc.sort();
-
-    return calc;
-  }, []);
+      return calc;
+    }, [])
+    .sort();
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
